3c6dbbc81ab637e5d1bdf1bd1b67a7208813e6b3
Luiz Chequini <luizchequini@gmail.com>
2021-02-22 09:56:36 -0300

    Criando serviços e tratamento de retorno da modal | Nossa primeira interface e dalvando os filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -23,6 +24,7 @@ import { RodapeComponent } from './shared/components/rodape/rodape.component';
     LayoutModule,
     MaterialModule,
     AppRoutingModule,
+    HttpClientModule
 
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FilmesService } from 'src/app/core/filmes.service';
 import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
+import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -12,7 +14,9 @@ export class CadastroFilmesComponent implements OnInit {
   cadastro: FormGroup;
   generos: Array<string>;
 
-  constructor(public validacao: ValidarCamposService, private fb: FormBuilder) { }
+  constructor(public validacao: ValidarCamposService,
+              private fb: FormBuilder,
+              private filmesService: FilmesService) { }
 
   get f(){
     return this.cadastro.controls;
@@ -33,17 +37,27 @@ export class CadastroFilmesComponent implements OnInit {
     this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção Científica', 'Comédia', 'Drama'];
   }
 
-  salvar(): void{
+  submit(): void{
     this.cadastro.markAllAsTouched();
     if(this.cadastro.invalid){
       return;
     }
 
-    alert('Sucesso: ' + JSON.stringify(this.cadastro.value, null, 4))
+    const filme = this.cadastro.getRawValue() as Filme;
+    this.salvar(filme);
   }
 
   reiniciarForm(): void{
     this.cadastro.reset();
+  }
+
+  private salvar(filme: Filme): void {
+    this.filmesService.salvar(filme).subscribe(()=>{
+      alert('Sucesso ao salvar!');
+    },
+    ()=>{
+      alert('Erro ao salvar!');
+    });
   }
 
 }
